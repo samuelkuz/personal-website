360541b3936976b4549b5e8540e77e8699c1463c
Sam Kuz <samuelkuz@gmail.com>
2021-03-17 17:01:33 -0700

    Add initial empty nav bar

---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./NavigationBar.scss";
+
+function NavigationBar() {
+
+    return (
+        <div className="nav-bar-container">
+            <div className="nav-bar-item">
+                Test
+            </div>
+        </div>
+    );
+}
+
+export default NavigationBar;
