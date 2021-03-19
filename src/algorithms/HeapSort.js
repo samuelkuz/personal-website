95f4c34b42afbc4c61cb6fbca67ad938be1f1ca1
Sam Kuz <samuelkuz@gmail.com>
2021-03-19 09:17:13 -0700

    Add working heap sort animations

--- src/algorithms/HeapSort.js
+++ src/algorithms/HeapSort.js
@@ -1,21 +1,26 @@
 function heapSort(arr) {
+    const animations = [];
+
     let length = arr.length;
 
     for (let i = Math.floor((length / 2 ) - 1); i >= 0; i--) {
-        heapify(arr, length, i);
+        heapify(arr, length, i, animations);
     }
 
     for (let i = length - 1; i > 0; i--) {
+        animations.push({type : "color", barOneIdx: 0, barTwoIdx: i, color: "red"});
+        animations.push({type : "swap", barOneIdx: 0, barTwoIdx: i});
+        animations.push({type : "color", barOneIdx: 0, barTwoIdx: i, color: "lightblue"});
         let temp = arr[0];
         arr[0] = arr[i];
         arr[i] = temp;
-        heapify(arr, i, 0);
+        heapify(arr, i, 0, animations);
     }
 
-    return arr;
+    return animations;
 }
 
-function heapify(arr, length, i) {
+function heapify(arr, length, i, animations) {
     let largest = i;
     let l = 2 * i + 1;
     let r = l + 1;
@@ -29,9 +34,14 @@ function heapify(arr, length, i) {
     }
 
     if (largest !== i) {
+        //Highlight
+        animations.push({type : "color", barOneIdx: i, barTwoIdx: largest, color: "red"});
         [arr[i], arr[largest]] = [arr[largest], arr[i]];
-
-        heapify(arr, length, largest);
+        // Move
+        animations.push({type : "swap", barOneIdx: i, barTwoIdx: largest});
+        //Un-Highlight
+        animations.push({type : "color", barOneIdx: i, barTwoIdx: largest, color: "lightblue"});
+        heapify(arr, length, largest, animations);
     }
     
     return arr;
--- src/pages/AlgorithmVisualizer.jsx
+++ src/pages/AlgorithmVisualizer.jsx
@@ -5,18 +5,61 @@ import { heapSort } from "../algorithms/HeapSort.js";
 import "./AlgorithmVisualizer.scss";
 
 function AlgorithmVisualizer() {
-    const [arr, setArr] = useState([]);
-    const [maxHeight, setMaxHeight] = useState(500);
-    const [size, setSize] = useState(25);
+    const ANIMATION_SPEED = 50;
+    const MAX_HEIGHT = 500;
+
+    const [animations, setAnimations] = useState([]);
+    const [bars, setBars] = useState([]);
+    const [barsInfo, setBarsInfo] = useState([]);
+    const [size, setSize] = useState(20);
     const [width, setWidth] = useState(20);
 
     useEffect(() => {
-        randomizeArray();
         initializeWidth();
         return function cleanup() {
             console.log("cleaning up");
         }
     }, []);
+
+    useEffect(() => {
+        randomizeArray();
+    }, [width]);
+
+    useEffect(() => {
+        if (animations.length > 0) {
+            const tempAnimations = [ ...animations ];
+            const animation = tempAnimations.shift();
+            switch (animation.type) {
+                case "color": 
+                    setTimeout(() => {
+                        const barOneIdx = animation.barOneIdx;
+                        const barTwoIdx = animation.barTwoIdx;
+                        const color = animation.color;
+                        const tempBarsInfo = JSON.parse(JSON.stringify(barsInfo));
+                        tempBarsInfo[barOneIdx].backgroundColor = color;
+                        tempBarsInfo[barTwoIdx].backgroundColor = color;
+                        setBarsInfo(tempBarsInfo);
+                        setAnimations(tempAnimations);
+                    }, ANIMATION_SPEED);
+                    break;
+                case "swap":
+                    setTimeout(() => {
+                        const barOneIdx = animation.barOneIdx;
+                        const barTwoIdx = animation.barTwoIdx;
+                        const tempBarsInfo = JSON.parse(JSON.stringify(barsInfo));
+                        const tempBars = [ ...bars ];
+                        [tempBarsInfo[barOneIdx], tempBarsInfo[barTwoIdx]] = [tempBarsInfo[barTwoIdx], tempBarsInfo[barOneIdx]];
+                        [tempBars[barOneIdx], tempBars[barTwoIdx]] = [tempBars[barTwoIdx], tempBars[barOneIdx]];
+                        setBars(tempBars);
+                        setBarsInfo(tempBarsInfo);
+                        setAnimations(tempAnimations);
+                    }, ANIMATION_SPEED);
+                    break;
+            }
+        } else {
+            console.log(bars);
+        }
+    }, [animations]);
 
     const initializeWidth = () => {
         // left + right margin in px
@@ -27,17 +70,27 @@ function AlgorithmVisualizer() {
     };
 
     const randomizeArray = () => {
-        let tempArr = [];
+        let tempBars = [];
+        let tempStyles = [];
         for (let i = 0; i < size; i++) {
-            tempArr.push(Math.floor(Math.random() * maxHeight));
+            const randomVal = Math.floor(Math.random() * MAX_HEIGHT);
+            const initStyle = {
+                height: `${randomVal}px`,
+                width: `${width}px`,
+                fontSize: `${width/2}px`,
+                backgroundColor: "lightblue",
+            }
+            tempBars.push(randomVal);
+            tempStyles.push(initStyle);
         }
-        setArr(tempArr);
+        setBars(tempBars);
+        setBarsInfo(tempStyles);
     };
 
     const handleSort = () => {
-        const tempArr = [ ...arr ];
-        const sorted = heapSort(tempArr);
-        setArr(sorted);
+        const tempArr = [ ...bars ];
+        const sortingAnimations = heapSort(tempArr);
+        setAnimations(sortingAnimations);
     }
 
     return (
@@ -45,12 +98,8 @@ function AlgorithmVisualizer() {
             <button onClick={handleSort}>Test</button>
             <div className="algorithm-bar-wrapper">
                 {
-                    arr.map((item,) => {
-                        const style = {
-                            height: `${item}px`,
-                            width: `${width}px`,
-                            fontSize: `${width/2}px`,
-                        };
+                    bars.map((item, idx) => {
+                        const style = barsInfo[idx];
                         return (
                             <div className="algorithm-bar" key={Math.random() * 10000} style={style}>{item}</div>
                         );
