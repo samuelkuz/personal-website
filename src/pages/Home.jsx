12e32172b55e73f547a01548da222dc04553fca6
Sam Kuz <samuelkuz@gmail.com>
2021-03-26 11:51:19 -0700

    Add updated styling and README

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { useHistory } from "react-router-dom";
 
 import headshot from "../assets/headshot.png";
@@ -6,14 +6,37 @@ import headshot from "../assets/headshot.png";
 import "./Home.scss";
 
 function Home() {
-    const introTitle = "Hello, I'm Sam - A Software Engineer from Minnesota.";
-    const introText = "I'm a newly graduated Computer Science student excited to begin my Software Engineering journey. I’ve developed features for millions of students on the PebbleGo team, worked on creating multiple in-house software applications, and have never been more motivated/excited to learn!";
+    const [showCopy, setShowCopy] = useState(false);
 
+    const emailRef = useRef();
     const history = useHistory();
+
+    const introTitle = "Hello, I'm Sam - A Software Engineer from Minnesota.";
+    const introText = "I'm a newly graduated Computer Science student who is excited to begin my Software Engineering journey. I’ve developed features for millions of students on the PebbleGo team, worked on creating multiple in-house software applications, and have never been more motivated/excited to learn!";
+
+    const calculateStyle = () => {
+        const style = {
+            left: emailRef.current.getBoundingClientRect().x + 5,
+            top: emailRef.current.getBoundingClientRect().y - 30,
+        };
+
+        return style;
+    }
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText("samuelkuz@gmail.com");
+        console.log(emailRef.current.getBoundingClientRect());
+        setShowCopy(true);
+        setTimeout(() => {
+            setShowCopy(false);
+        }, 600);
+    };
 
     const handleRedirect = (redirect) => {
         history.push(`/${redirect}`);
     };
+    
+    const projectsTitle = "Projects & Featured Work.";
 
     return (
         <div className="home-wrapper">
@@ -26,8 +49,14 @@ function Home() {
                     <img className="head-shot" src={headshot} alt="Headshot" />
                 </div>
             </div>
-            <div className="projects-link" onClick={() => handleRedirect("algorithm-visualizer")}>
-                CLICK HERE FOR PROJECTS!
+            {showCopy &&
+                <div className="copy-popup" style={calculateStyle()}>Copied to clipboard!</div>}
+            <div className="copy-email" ref={emailRef} onClick={() => handleCopy()}>samuelkuz@gmail.com</div>
+            <div className="projects-wrapper">
+                <div className="projects-title">{projectsTitle}</div>
+                <div className="projects-link" onClick={() => handleRedirect("algorithm-visualizer")}>
+                    CLICK HERE FOR PROJECTS!
+                </div>
             </div>
         </div>
     );
